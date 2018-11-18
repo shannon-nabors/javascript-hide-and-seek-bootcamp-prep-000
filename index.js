bbe21bab7d4f8457aaf2d241d6320ceb5a7166d2
Shannon Nabors <shannon.nabors@gmail.com>
2018-11-18 21:14:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+/*function getFirstSelector(selector) {
+  var selection = (document.querySelector(selector));
+  return(selection);
+}*/
 
+function getFirstSelector(selector) {
+  var selection = (document.querySelector(selector));
+  return(selection);
+}
+
+function nestedTarget() {
+  var nested = (document.getElementById('nested'));
+  var target = (document.querySelector('#nested .target'));
+  return target;
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+  const firstList = rankedLists[0];
+  const secondList = rankedLists[1];
+  
+  for (let i = 0; i < firstList.children.length; i++) {
+    var currentRank = (parseInt(firstList.children[i].innerHTML));
+    var newRank = currentRank + n;
+    firstList.children[i].innerHTML = parseInt(newRank);
+  }
+      
+  for (let i = 0; i < secondList.children.length; i++) {
+    var cRank = (parseInt(secondList.children[i].innerHTML));
+    var nRank = cRank + n;
+    secondList.children[i].innerHTML = parseInt(nRank);
+  }
+}
+
+function deepestChild() {
+  var deepChild = (document.querySelector('#grand-node div div div div'));
+  return deepChild;
+}
