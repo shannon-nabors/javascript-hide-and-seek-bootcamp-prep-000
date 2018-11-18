cc7618e2b9e4e6a3ba6c8c214b50688cf0bc3207
Shannon Nabors <shannon.nabors@gmail.com>
2018-11-18 20:17:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+/*function getFirstSelector(selector) {
+  var selection = (document.querySelector(selector));
+  return(selection);
+}*/
 
+function getFirstSelector(selector) {
+  var selection = (document.querySelector(selector));
+  return(selection);
+}
+
+function nestedTarget() {
+  var nested = (document.getElementById('nested'));
+  var target = (document.querySelector(nested.target));
+  return target;
+}
+
+function increaseRankBy(n) {
+  
+}
+
+function deepestChild() {
+  
+}
