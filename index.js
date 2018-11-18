437124777174546d8c915f6b2353137393552408
Shannon Nabors <shannon.nabors@gmail.com>
2018-11-18 20:45:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,9 +34,6 @@ function increaseRankBy(n) {
     }
 }
 
-const rankedLists = document.querySelectorAll('.ranked-list');
-console.log(rankedLists);
-
 function deepestChild() {
   var deepChild = (document.querySelector('#grand-node div div div div'));
   return deepChild;
